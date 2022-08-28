44133c8682553c2e7a6a7fa8379ea0a46b73f242
Bryguy76 <bryant.bourgeois@comcast.net>
2022-08-28 14:05:05 -0600

    Add verbose, repetitive, keyboard support

--- script.js
+++ script.js
@@ -137,3 +137,72 @@ backspace.addEventListener('click', function () {
     displayUpdate(operand2);
   }
 });
+
+//Handle keyboard input
+
+window.addEventListener('keydown', e => {
+  let code = e.key;
+  if (code == 0 || code <= 9) {
+    let numContent = code;
+    if (opSwitch) {
+      operand1 += numContent;
+      displayUpdate(operand1);
+    } else {
+      operand2 += numContent;
+      displayUpdate(operand2);
+    }
+  } else if (code == '.') {
+    let dotContent = '.';
+    if (opSwitch) {
+      operand1 += dotContent;
+      displayUpdate(operand1);
+      dot.disabled = true;
+    } else {
+      operand2 += dotContent;
+      displayUpdate(operand2);
+      dot.disabled = true;
+    }
+  } else if (code == 'Backspace') {
+    if (opSwitch) {
+      operand1 = operand1.slice(0, operand1.length - 1);
+      displayUpdate(operand1);
+    } else {
+      operand2 = operand2.slice(0, operand2.length - 1);
+      displayUpdate(operand2);
+    }
+  } else if (code == '+' || code == '-' || code == '*' || code == '/') {
+    if (!operand1) {
+      return;
+    }
+    if (opSwitch) {
+      opSwitch = false;
+      op = code;
+      displayUpdate('');
+      dot.disabled = false;
+      return;
+    } else {
+      output = operate(op, operand1, operand2);
+      displayUpdate(output);
+      operand1 = output;
+      operand2 = '';
+      op = code;
+      dot.disabled = false;
+      return;
+    }
+  } else if (code == 'a' || code == 'c') {
+    allClear();
+  } else if (code == 'Enter') {
+    if (opSwitch) {
+      return;
+    }
+    output = operate(op, operand1, operand2);
+    displayUpdate(output);
+    operand1 = output;
+    operand2 = '';
+    op = '';
+    opSwitch = true;
+    dot.disabled = false;
+    return;
+  }
+  console.log(code);
+});
